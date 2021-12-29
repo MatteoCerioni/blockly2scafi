bdfb8d667d3ec961d1b5773ab32871ec3fece6e1
Matteo Cerioni <m.cerioni@ithings.it>
2021-12-29 18:08:46 +0100

    Move blocks json definition from scafi_generator.js to blocks.js file

---
+++ src/main/resources/blocks.js
@@ -0,0 +1,18 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "write",
+        "message0": "Write %1",
+        "args0": [
+            {
+                "type": "field_input",
+                "name": "MESSAGE",
+                "text": "Hello Blockly"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 165,
+        "tooltip": "Write",
+        "helpUrl": ""
+    }
+]);
--- src/main/resources/scafi_generator.js
+++ src/main/resources/scafi_generator.js
@@ -1,29 +1,6 @@
-Blockly.defineBlocksWithJsonArray([
-    {
-        "type": "write",
-        "message0": "Write %1",
-        "args0": [
-            {
-                "type": "field_input",
-                "name": "MESSAGE",
-                "text": "Hello Blockly"
-            }
-        ],
-        "previousStatement": null,
-        "nextStatement": null,
-        "colour": 165,
-        "tooltip": "Write",
-        "helpUrl": ""
-    }
-]);
-
-
 const scafiGenerator = new Blockly.Generator('ScaFi');
-scafiGenerator['write'] = function(block){
-    const textValue = block.getFieldValue('MESSAGE');
-    const code = '"' + textValue + '"';
-    return code;
+scafiGenerator['write'] = function (block) {
+    return '"' + block.getFieldValue('MESSAGE') + '"';
 }
 
 Blockly.ScaFi = scafiGenerator;
-
