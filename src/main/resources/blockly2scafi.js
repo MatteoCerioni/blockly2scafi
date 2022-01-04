8a90a7b9fbb42c513006345dfb25c133f96479b6
Matteo Cerioni <m.cerioni@ithings.it>
2022-01-04 19:27:22 +0100

    add int, bool, tuple and sense blocks

--- src/main/resources/blockly2scafi.js
+++ src/main/resources/blockly2scafi.js
@@ -3,6 +3,16 @@ Blockly.createBlockly2ScafiWorkspace = function (elt) {
         '<xml>' +
         '<block type="output"></block>' +
         '<block type="string"></block>' +
+        '<block type="integer"></block>' +
+        '<block type="boolean"></block>' +
+        '<block type="tuple"></block>' +
+        '<block type="sense">' +
+        '<value name="SENSOR_NAME">' +
+        '<shadow type="string">' +
+        '<field name="STRING_VALUE">sensor1</field>' +
+        '</shadow>' +
+        '</value>' +
+        '</block>' +
         '</xml>'
 
     const initialWorkspaceXml =
@@ -27,6 +37,8 @@ Blockly.createBlockly2ScafiWorkspace = function (elt) {
 }
 
 const scafiGenerator = new Blockly.Generator('ScaFi');
+scafiGenerator.PRECEDENCE = 0;
+scafiGenerator.FUNCTION_CALL = 1;
 
 scafiGenerator['aggregate_program'] = function (block) {
     var import_code = "";
@@ -37,11 +49,47 @@ scafiGenerator['aggregate_program'] = function (block) {
 }
 
 scafiGenerator['string'] = function (block) {
-    return ['"' + block.getFieldValue('STRING_VALUE') + '"', 0]; //TODO USE ORDER
+    return ['"' + block.getFieldValue('STRING_VALUE') + '"', 0];
+}
+scafiGenerator['integer'] = function (block) {
+    const code = block.getFieldValue('INTEGER_VALUE').toString();
+    return [code, scafiGenerator.PRECEDENCE];
 }
 
-scafiGenerator['output'] = function (block) {
-    return Blockly.ScaFi.valueToCode(block, "OUTPUT_VALUE", 0); //TODO USE ORDER
+scafiGenerator['boolean'] = function (block) {
+    const code = block.getFieldValue('BOOLEAN_VALUE');
+    return [code, scafiGenerator.PRECEDENCE];
 }
+
+scafiGenerator['tuple'] = function (block) {
+    const values = [
+        Blockly.ScaFi.valueToCode(block, "VALUE_1", scafiGenerator.PRECEDENCE),
+        Blockly.ScaFi.valueToCode(block, "VALUE_2", scafiGenerator.PRECEDENCE)
+    ];
+
+    const code = "(" + values[0] + "," + values[1] + ")"
+    return [code, scafiGenerator.PRECEDENCE];
+}
+
+scafiGenerator['sense'] = function (block) {
+    const sensor = Blockly.ScaFi.valueToCode(block, 'SENSOR_NAME', scafiGenerator.PRECEDENCE);
+    const code = 'sense(' + sensor + ')';
+    return [code, scafiGenerator.PRECEDENCE];
+}
+
+
+scafiGenerator['output'] = function (block) {
+    return Blockly.ScaFi.valueToCode(block, "OUTPUT_VALUE", scafiGenerator.PRECEDENCE);
+}
+
+scafiGenerator.scrub_ = function (block, code, opt_thisOnly) {
+    const nextBlock =
+        block.nextConnection && block.nextConnection.targetBlock();
+    let nextCode = '';
+    if (nextBlock) {
+        nextCode = opt_thisOnly ? '' : '\n' + scafiGenerator.blockToCode(nextBlock);
+    }
+    return code + nextCode;
+};
 
 Blockly.ScaFi = scafiGenerator;
--- src/main/resources/blocks_library.js
+++ src/main/resources/blocks_library.js
@@ -24,6 +24,7 @@ Blockly.defineBlocksWithJsonArray([{
             }
         ],
         "previousStatement": null,
+        "nextStatement": null,
         "colour": 230,
         "tooltip": "Output a value",
         "helpUrl": ""
@@ -42,5 +43,78 @@ Blockly.defineBlocksWithJsonArray([{
         "colour": 165,
         "tooltip": "",
         "helpUrl": ""
-    }
+    }, {
+        "type": "integer",
+        "message0": "%1",
+        "args0": [
+            {
+                "type": "field_number",
+                "name": "INTEGER_VALUE",
+                "value": 0,
+                "precision": 1
+            }
+        ],
+        "output": "Integer",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "boolean",
+        "message0": "%1",
+        "args0": [
+            {
+                "type": "field_dropdown",
+                "name": "BOOLEAN_VALUE",
+                "options": [
+                    [
+                        "true",
+                        "TRUE"
+                    ],
+                    [
+                        "false",
+                        "FALSE"
+                    ]
+                ]
+            }
+        ],
+        "output": "Boolean",
+        "colour": 210,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "tuple",
+        "message0": "( %1 , %2 )",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "VALUE_1"
+            },
+            {
+                "type": "input_value",
+                "name": "VALUE_2"
+            }
+        ],
+        "inputsInline": true,
+        "output": "Tuple",
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    }, {
+        "type": "sense",
+        "message0": "Sense %1",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "SENSOR_NAME",
+                "check": "String"
+            }
+        ],
+        "inputsInline": false,
+        "output": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
 ]);
